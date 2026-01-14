d49d6457ec2d3dfb130b7cdf68a266ecee014d6c
Petro Mushenyk <petro.mushenyk.ub@bp.renesas.com>
2026-01-14 17:34:45 +0200

    Added original 'rm_pmgr_tcp_client_sleep4_example_ek_ra6w1_ep' code
    Signed-off-by: Petro Mushenyk <petro.mushenyk.ub@bp.renesas.com>

---
+++ example_projects/devel/rm_pmgr_tcp_client_sleep4_example_ek_ra6w1_ep/src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define ENABLE_TCP_TX              1 /* 0 for disable */
+
+#define TCP_SERVER_IP              "10.70.122.67"
+#define TCP_SERVER_PORT            12345
+
+#define TX_DELAY_MS                10000 /* 10sec delay */
+
+#endif // CONFIG_H_
